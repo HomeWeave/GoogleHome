cb53fde73cfbec6b7a345e1b678de84072c1a0bd
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2021-06-07 23:04:29 +0530

    Add support media states of Chromecast devices.

--- anton_google_home/service.py
+++ anton_google_home/service.py
@@ -11,6 +11,7 @@ from anton.plugin_pb2 import PipeType, IOT_INSTRUCTION, IOT_EVENTS
 from anton.events_pb2 import GenericEvent
 from anton.device_pb2 import DEVICE_STATUS_ONLINE, DEVICE_KIND_STREAMING_STICK
 from anton.power_pb2 import POWER_OFF
+from anton.media_pb2 import PLAYING, PAUSED, STOPPED
 
 
 def online_event(device_id, friendly_name):
@@ -26,6 +27,51 @@ def online_event(device_id, friendly_name):
     return event
 
 
+def media_event(device_id, player_id, player_name, track_name, artist, url,
+                album_art, play_state):
+    event = GenericEvent(device_id=device_id)
+    if player_id:
+        event.media.media.player_id = player_id
+    if player_name:
+        event.media.media.player_name = player_name
+    if track_name:
+        event.media.media.track_name = track_name
+    if artist:
+        event.media.media.artist = artist
+    if url:
+        event.media.media.url = url
+    if album_art:
+        event.media.media.album_art = album_art
+
+    mapping = {"PLAYING": PLAYING, "PAUSED": PAUSED}
+    play_status = mapping.get(play_state, STOPPED)
+    event.media.media.play_status = play_status
+
+    return event
+
+
+class ChromecastController(object):
+    def __init__(self, device, send_event):
+        self.device = device
+        self.send_event = send_event
+        self.device.media_controller.register_status_listener(self)
+
+    def new_media_status(self, status):
+        media = self.device.media_controller
+
+        player_id = str(self.device.uuid)
+        player_name = str(self.device.device.friendly_name)
+        track_name = media.status.title
+        artist = media.status.artist
+        url = "(no URL)"
+        album_art = media.status.images and media.status.images[0].url
+        play_state = media.status.player_state
+
+        event = media_event(str(self.device.uuid), player_id, player_name,
+                            track_name, artist, url, album_art, play_state)
+        self.send_event(event)
+
+
 
 class AntonGoogleHomePlugin(AntonPlugin):
     def setup(self, plugin_startup_info):
@@ -34,22 +80,32 @@ class AntonGoogleHomePlugin(AntonPlugin):
         self.send_event = event_controller.create_client(0, self.on_response)
 
         registry = self.channel_registrar()
-        registry.register_controller(IOT_INSTRUCTION,
-                                     instruction_controller)
+        registry.register_controller(IOT_INSTRUCTION, instruction_controller)
         registry.register_controller(IOT_EVENTS, event_controller)
+
+        self.controllers = {}
 
     def on_start(self):
         self.discovery_thread = Thread(target=self.discover_chromecasts)
         self.discovery_thread.start()
 
     def discover_chromecasts(self):
-        devices, browser = pychromecast.get_chromecasts()
-        browser.stop_discovery()
+        while True:
+            devices, browser = pychromecast.get_chromecasts()
+            browser.stop_discovery()
 
-        for device in devices:
-            event = online_event(device.model_name + str(device.uuid),
-                                 device.device.friendly_name)
-            self.send_event(event)
+            for device in devices:
+                device_id = str(device.uuid)
+                if device_id in self.controllers:
+                    continue
+
+                device.wait()
+                controller = ChromecastController(device, self.send_event)
+                self.controllers[device_id] = controller
+
+                event = online_event(device.model_name + str(device.uuid),
+                                     device.device.friendly_name)
+                self.send_event(event)
 
     def on_stop(self):
         self.discovery_thread.join()
